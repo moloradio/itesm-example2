2223efb68bfeceaef9e1b81b31dbfd60ae435688
moloradio <aldo@macbook-pro-de-aldo.local>
2016-06-17 15:33:58 -0500

    Initial Commit

---
+++ itestm-test2Tests/itestm_test2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  itestm_test2Tests.swift
+//  itestm-test2Tests
+//
+//  Created by aldo ulises pena martinez on 17/06/16.
+//  Copyright © 2016 fatimsoft. All rights reserved.
+//
+
+import XCTest
+@testable import itestm_test2
+
+class itestm_test2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
