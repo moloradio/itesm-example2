8b4fc4dfaa696b5832bba7b1a518460151b3ed51
moloradio <aldo@macbook-pro-de-aldo.local>
2016-06-17 15:52:26 -0500

    Commit 1

--- itestm-test2/ViewController.swift
+++ itestm-test2/ViewController.swift
@@ -12,9 +12,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        serieCien()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func serieCien(){
+        
+        for i in 0...100 {
+            
+            if i%5 == 0 {
+                print("# \(i) Bingo!!!")
+            }else{
+                if i >= 30 && i <= 40 {
+                    print("# \(i) Viva Swift!!!")
+                }else{
+                    if i%2 == 0 {
+                        print("# \(i) Par!!!")
+                    }else{
+                        print("# \(i) Impar!!!")
+                    }
+                }
+            }
+            
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
